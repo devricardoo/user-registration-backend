b6b41a877b19e955f22a4ae37ef6f1f5cd879920
Paulo Ricardo <paulorihcardo18@gmail.com>
2025-05-14 22:34:17 -0300

    metodo create

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -13,8 +14,54 @@ class AuthController extends Controller
             'except' => [
                 'login',
                 'create',
-                'unauthorized'
+                'unauthorized',
+                'user',
             ]
         ]);
+    }
+
+    public function unauthorized()
+    {
+        return response()->json([
+            'error' => 'Unauthorized'
+        ]);
+    }
+
+    public function create(Request $request)
+    {
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|string|email|max:100|unique:users,email',
+            'password' => 'required|string|min:4',
+            'cpf' => 'required|string|min:11|unique:users,cpf',
+        ];
+
+        $feedback = [
+            'required' => 'O campo :attribute é obrigatório',
+            'unique' => 'O campo :attribute já existe',
+            'email' => 'O campo :attribute precisa ser um email válido',
+            'min' => 'O campo :attribute precisa ter no mínimo :min caracteres',
+        ];
+
+        $request->validate($rules, $feedback);
+
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'cpf' => $request->cpf,
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = Auth::attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user
+        ], 200);
     }
 }
