767e84c37bb24d4bcea42015f008f70c02dd0953
Paulo Ricardo <paulorihcardo18@gmail.com>
2025-05-15 05:35:23 -0300

    criacao das apis

--- routes/api.php
+++ routes/api.php
@@ -14,17 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/404', 'App\Http\Controllers\AuthController@unauthorized');
 
 Route::post('/auth/login', 'App\Http\Controllers\AuthController@login');
 Route::post('/auth/logout', 'App\Http\Controllers\AuthController@logout');
 Route::post('/auth/refresh', 'App\Http\Controllers\AuthController@refresh');
+Route::post('/user', 'App\Http\Controllers\AuthController@create');
 
 Route::get('/user', 'App\Http\Controllers\UserController@index');
-Route::post('/user', 'App\Http\Controllers\UserController@create');
 Route::put('/user/{id}', 'App\Http\Controllers\UserController@update');
+Route::patch('/user/{id}', 'App\Http\Controllers\UserController@update');
 Route::delete('/user/{id}', 'App\Http\Controllers\UserController@delete');
+
+Route::post('/profile', 'App\Http\Controllers\UserController@createprofile');
+Route::post('/profile/{id}', 'App\Http\Controllers\ProfileController@updateprofile');
+Route::delete('/profile/{id}', 'App\Http\Controllers\ProfileController@deleteprofile');
+
+Route::get('/address', 'App\Http\Controllers\AddressController@index');
+Route::post('/address', 'App\Http\Controllers\AddressController@createaddress');
+Route::put('/address/{id}', 'App\Http\Controllers\AddressController@updateaddress');
+Route::patch('/address/{id}', 'App\Http\Controllers\AddressController@updateaddress');
+Route::delete('/address/{id}', 'App\Http\Controllers\AddressController@deleteaddress');
