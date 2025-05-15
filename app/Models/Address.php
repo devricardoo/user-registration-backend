f9ae8ee21df2f717107056c9e331e31425dfeca1
Paulo Ricardo <paulorihcardo18@gmail.com>
2025-05-15 05:35:01 -0300

    adiçao fillable

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -11,8 +11,27 @@ class Address extends Model
 
     protected $table = 'addresses';
 
+    protected $fillable = ['public_place', 'cep'];
+
     public function users()
     {
         return $this->belongsToMany(User::class, 'addresses_users');
+    }
+
+    public function rules()
+    {
+        return [
+            'public_place' => 'required',
+            'cep' => 'required|regex:/^\d{5}-?\d{3}$/|max:8',
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'max' => 'O campo :attribute precisa ter no máximo :max caracteres',
+            'regex' => 'Informe um CEP válido',
+        ];
     }
 }
